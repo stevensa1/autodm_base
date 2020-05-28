5b40b2fdb55eebcf8d4617b761be67681708aba5
Steven Soewignyo <54796666+stevensa1@users.noreply.github.com>
2020-05-28 15:51:27 +0700

    Change trigger prikitiw to sdf!

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "prikitiw" in message:
-                        message = message.replace("prikitiw", "")
+                    if "sdf!" in message:
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
