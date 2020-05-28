1cbd938bffb55098a6fbecc794b343da567be0ed
Steven Soewignyo <54796666+stevensa1@users.noreply.github.com>
2020-05-28 16:01:28 +0700

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -10,7 +10,7 @@ from async_upload import VideoTweet
 class Twitter:
     def __init__(self):
         print("initializing twitter....")
-        self.inits = tweepy.OAuthHandler(constants.CONSUMER_KEY, constants.CONSUMER_SCRET)
+        self.inits = tweepy.OAuthHandler(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
         self.inits.set_access_token(constants.ACCESS_KEY, constants.ACCESS_SECRET)
         self.api = tweepy.API(self.inits)
 
