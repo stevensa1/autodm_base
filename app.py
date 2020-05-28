cdabfeba7ef04f75301e852e5b801f85e36f4c11
Steven Soewignyo <54796666+stevensa1@users.noreply.github.com>
2020-05-28 17:21:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "sdf!" or "Sdf!" or "ges!" in message:
+                    if "sdf!" in message:
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
